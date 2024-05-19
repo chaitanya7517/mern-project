594d2926c8c5817b19b9296a5b36630ed3129c2b
chaitanya7517 <chaitanyanalawade84@gmail.com>
2024-05-19 21:57:54 +0530

    creating test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 const app= express();
@@ -11,18 +12,9 @@ mongoose.connect(process.env.MONGO).then(()=> {
     console.log('error while connection with db');
 })
 
-//method 2
-// const connectDB = async function() {
-//     try {
-//         await mongoose.connect(process.env.MONGO);
-//         console.log('connected to db');
-//     } catch(error) {
-//         console.log('Error while connecting with db');
-//     }
-// }
-// connectDB();
-
 
 app.listen(3000,()=>{
     console.log(`server is running on 3000 `);
 })
+
+app.use('/api/user',userRouter);
